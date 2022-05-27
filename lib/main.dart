25ae3fc621183a7d7dfbb5366d774c28828c55a7
Lakshy-1 <lvikramsah@gmail.com>
2022-05-27 09:57:31 +0545

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_routing/screens/homepage.dart';
-import './router/router_handler.dart';
+import 'package:flutter_routing/anonymousRouting/homePage.dart';
+import 'package:flutter_routing/generatedRouting/homepage.dart';
+import 'package:flutter_routing/namedRoutes/fourthScreen.dart';
+import 'package:flutter_routing/namedRoutes/homePage.dart';
+
+import 'generatedRouting/router/router_handler.dart';
+import 'namedRoutes/firstScreen.dart';
+import 'namedRoutes/secondScreen.dart';
+import 'namedRoutes/thirdScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +21,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      onGenerateRoute: generateRoute,
-      home: const HomePage(),
+      //NamedRoute
+      routes: {
+        '/': (context) => NamedHomePage(),
+        '/first': (context) => NFirstScreen(),
+        '/second': (context) => NSecondScreen(),
+        '/third': (context) => NThirdScreen(),
+        '/fourth': (context) => NFouthScreen(params: "params")
+      },
+      //generatedRouting
+      //onGenerateRoute: generateRoute,
+      // home:
+      //     //anonymousRouting
+      //     //const AHomePage()
+      //     //NamedRoutes
+      //     const NamedHomePage()
+      //GeneratedRoutes
+      // const GeneratedHomePage(),
     );
   }
 }
