db80db4fa7e2ace9f1860d175a9ccf948c5d7412
Lakshy-1 <lvikramsah@gmail.com>
2022-05-27 09:59:29 +0545

    removed

--- lib/router/app_routes.dart
+++
@@ -1,7 +0,0 @@
-abstract class Routes {
-  static const String home = 'home';
-  static const String first = 'first';
-  static const String second = 'second';
-  static const String third = 'third';
-  static const String fourth = 'fourth';
-}
